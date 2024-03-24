1209e610e358742bdee7e5c2f33ee797e63c4e0f
lindgeci <lg69462@ubt-uni.net>
2024-03-24 16:03:52 +0100

    update per te gjitha tabelat dhe modeli fizik per reference

--- HMS.sql
+++ HMS.sql
@@ -16,6 +16,15 @@ CREATE TABLE Patient (
     Discharge_Date DATE,
     Phone VARCHAR(20)
 );
+--========================================================================
+--tabela Department   u ekzekutua
+
+CREATE TABLE Department (
+    Dept_ID INT PRIMARY KEY NOT NULL,
+    Dept_head VARCHAR(100),
+    Dept_name VARCHAR(100) NOT NULL,
+    Emp_Count INT
+);
 
 --====================================
 
@@ -45,41 +54,41 @@ CREATE TABLE Doctor (
     Qualifications VARCHAR(255) NOT NULL,
     Emp_ID INT NOT NULL,
     Specialization VARCHAR(100),
-    FOREIGN KEY (Emp_ID) REFERENCES Employee(Emp_ID)
+    FOREIGN KEY (Emp_ID) REFERENCES Staff(Emp_ID)
 );
 
 --============================
 --Tabela Nurse
 
-CREATE TABLE Nurse (
-    Nurse_ID INT PRIMARY KEY NOT NULL,
-    Patient_ID INT NOT NULL,
-    Emp_ID INT NOT NULL,
-    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID),
-    FOREIGN KEY (Emp_ID) REFERENCES Employee(Emp_ID)
-);
+--CREATE TABLE Nurse (
+--    Nurse_ID INT PRIMARY KEY NOT NULL,
+--    Patient_ID INT NOT NULL,
+--    Emp_ID INT NOT NULL,
+--    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID),
+--    FOREIGN KEY (Emp_ID) REFERENCES Staff(Emp_ID)
+--);
 
 -----=============================
 --tabela Room
 
-CREATE TABLE Room (
-    Room_ID INT PRIMARY KEY AUTO_INCREMENT,
-    Room_type VARCHAR(20) NOT NULL,
-    Patient_ID INT,
-    Room_cost DECIMAL(10, 2),
-    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID)
-);
+--CREATE TABLE Room (
+--    Room_ID INT PRIMARY KEY not null,
+--    Room_type VARCHAR(20) NOT NULL,
+--    Patient_ID INT,
+--    Room_cost DECIMAL(10, 2),
+--    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID)
+--);
 
 --===================================================
 
 --Tabela Medicine 
 
-CREATE TABLE Medicine (
-    Medicine_ID INT PRIMARY KEY NOT NULL,
-    M_name VARCHAR(100) NOT NULL,
-    M_Quantity INT NOT NULL,
-    M_Cost DECIMAL(10, 2) NOT NULL
-);
+--CREATE TABLE Medicine (
+--    Medicine_ID INT PRIMARY KEY NOT NULL,
+--    M_name VARCHAR(100) NOT NULL,
+--    M_Quantity INT NOT NULL,
+--    M_Cost DECIMAL(10, 2) NOT NULL
+--);
 
 
 --========================================
@@ -91,12 +100,12 @@ CREATE TABLE Insurance (
     Ins_Code VARCHAR(10) NOT NULL,
     End_Date DATE,
     Provider VARCHAR(100) NOT NULL,
-    Plan VARCHAR(100) NOT NULL,
+    Plan_ VARCHAR(100) NOT NULL,
     Co_Pay DECIMAL(10, 2),
     Coverage VARCHAR(255),
-    Maternity BOOLEAN,
-    Dental BOOLEAN,
-    Optical BOOLEAN,
+    Maternity char(1) check(Maternity='true' or Maternity ='false'),
+    Dental char(1) check(Dental='true' or Dental ='false'),
+    Optical char(1) check(Optical='true' or Optical ='false'),
     FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID)
 );
 
@@ -104,106 +113,99 @@ CREATE TABLE Insurance (
 --==========================================================
 --Tabela BILL
 
-CREATE TABLE BILL (
-    Payment_ID INT PRIMARY KEY NOT NULL,
-    Date DATE NOT NULL,
-    Room_cost DECIMAL(10, 2),
-    Other_charges DECIMAL(10, 2),
-    M_Cost DECIMAL(10, 2),
-    Total DECIMAL(10, 2) NOT NULL,
-    Patient_ID INT NOT NULL,
-    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID),
-    FOREIGN KEY (Room_cost) REFERENCES Room(Room_cost),
-    FOREIGN KEY (M_Cost) REFERENCES Medicine(M_Cost)
-);
+--CREATE TABLE BILL (
+--    Payment_ID INT PRIMARY KEY NOT NULL,
+--    Date DATE NOT NULL,
+--    Room_cost DECIMAL(10, 2),
+--    Other_charges DECIMAL(10, 2),
+--    M_Cost DECIMAL(10, 2),
+--    Total DECIMAL(10, 2) NOT NULL,
+--    Patient_ID INT NOT NULL,
+--	Room_ID INT NOT NULL,
+--	Medicine_ID INT NOT NULL,
+--    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID),
+--    FOREIGN KEY (Room_ID) REFERENCES Room(Room_ID),
+--    FOREIGN KEY (Medicine_ID) REFERENCES Medicine(Medicine_ID)
+--);
 --===========================================================
 --tabela Prescription 
 
-CREATE TABLE Prescription (
-    Prescription_ID INT PRIMARY KEY NOT NULL,
-    Patient_ID INT NOT NULL,
-    Date DATE NOT NULL,
-    Dosage VARCHAR(100) NOT NULL,
-    Doctor_ID INT NOT NULL,
-    Medicine_ID INT NOT NULL,
-    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID),
-    FOREIGN KEY (Doctor_ID) REFERENCES Doctor(Doctor_ID),
-    FOREIGN KEY (Medicine_ID) REFERENCES Medicine(Medicine_ID)
-);
+--CREATE TABLE Prescription (
+--    Prescription_ID INT PRIMARY KEY NOT NULL,
+--    Patient_ID INT NOT NULL,
+--   Date DATE NOT NULL,
+--    Dosage VARCHAR(100) NOT NULL,
+--    Doctor_ID INT NOT NULL,
+--    Medicine_ID INT NOT NULL,
+--    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID),
+--    FOREIGN KEY (Doctor_ID) REFERENCES Doctor(Doctor_ID),
+--    FOREIGN KEY (Medicine_ID) REFERENCES Medicine(Medicine_ID)
+--);
 
 --=====================================================================
 --tabela Appointment
 
-CREATE TABLE Appointment (
-    Appoint_ID INT PRIMARY KEY NOT NULL,
-    Scheduled_On DATE NOT NULL,
-    Date DATE NOT NULL,
-    Time TIME NOT NULL,
-    Doctor_ID INT NOT NULL,
-    Patient_ID INT NOT NULL,
-    FOREIGN KEY (Doctor_ID) REFERENCES Doctor(Doctor_ID),
-    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID)
-);
+--CREATE TABLE Appointment (
+--    Appoint_ID INT PRIMARY KEY NOT NULL,
+--    Scheduled_On DATE NOT NULL,
+--    Date DATE NOT NULL,
+--    Time TIME NOT NULL,
+--    Doctor_ID INT NOT NULL,
+--    Patient_ID INT NOT NULL,
+--    FOREIGN KEY (Doctor_ID) REFERENCES Doctor(Doctor_ID),
+--    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID)
+--);
 
---========================================================================
---tabela Department   u ekzekutua
-
-CREATE TABLE Department (
-    Dept_ID INT PRIMARY KEY NOT NULL,
-    Dept_head VARCHAR(100),
-    Dept_name VARCHAR(100) NOT NULL,
-    Emp_Count INT
-);
 
 
 --=========================================================================
 --Tabela Payroll
 
-CREATE TABLE Payroll (
-    Account_no INT PRIMARY KEY,
-    Salary DECIMAL(10, 2) NOT NULL,
-    Bonus DECIMAL(10, 2),
-    Emp_ID INT NOT NULL,
-    IBAN VARCHAR(50),
-    FOREIGN KEY (Emp_ID) REFERENCES Employee(Emp_ID)
-);
+--CREATE TABLE Payroll (
+--    Account_no INT PRIMARY KEY,
+--    Salary DECIMAL(10, 2) NOT NULL,
+--    Bonus DECIMAL(10, 2),
+--    Emp_ID INT NOT NULL,
+--    IBAN VARCHAR(50),
+--    FOREIGN KEY (Emp_ID) REFERENCES Staff(Emp_ID)
+--);
 
 
 -- =====================================================
 --Tabela Emergency_contact
 
-CREATE TABLE Emergency_contact (
-    Contact_ID INT PRIMARY KEY,
-    Contact_Name VARCHAR(100) NOT NULL,
-    Phone VARCHAR(20) NOT NULL,
-    Relation VARCHAR(50) NOT NULL,
-    Patient_ID INT NOT NULL,
-    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID)
-);
+--CREATE TABLE Emergency_contact (
+--    Contact_ID INT PRIMARY KEY,
+--    Contact_Name VARCHAR(100) NOT NULL,
+--    Phone VARCHAR(20) NOT NULL,
+--    Relation VARCHAR(50) NOT NULL,
+--    Patient_ID INT NOT NULL,
+--    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID)
+--);
 
 
 --==================================
 --Tablea Medical History
 
-CREATE TABLE Medical_History (
-    Record_ID INT PRIMARY KEY,
-    Patient_ID INT NOT NULL,
-    Allergies VARCHAR(255),
-    Pre_Conditions VARCHAR(255),
-    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID)
-);
+--CREATE TABLE Medical_History (
+--    Record_ID INT PRIMARY KEY,
+--    Patient_ID INT NOT NULL,
+--    Allergies VARCHAR(255),
+--    Pre_Conditions VARCHAR(255),
+--    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID)
+--);
 
 
 --======================================
 --tabela Lab Screeining  u ekzekutua
 
-CREATE TABLE Lab_Screening (
-    Lab_ID INT PRIMARY KEY,
-    Patient_ID INT NOT NULL,
-    Technician_ID INT,
-    Doctor_ID INT,
-    Test_Cost DECIMAL(10, 2),
-    Date DATE,
-    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID),
-    FOREIGN KEY (Doctor_ID) REFERENCES Doctor(Doctor_ID)
-);
+--CREATE TABLE Lab_Screening (
+--    Lab_ID INT PRIMARY KEY,
+--   Patient_ID INT NOT NULL,
+--    Technician_ID INT,
+--    Doctor_ID INT,
+--    Test_Cost DECIMAL(10, 2),
+--    Date DATE,
+--    FOREIGN KEY (Patient_ID) REFERENCES Patient(Patient_ID),
+--    FOREIGN KEY (Doctor_ID) REFERENCES Doctor(Doctor_ID)
+--);
